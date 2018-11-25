eaf50011535d42cc75836c4578fe75b94f78301e
= <=>
2018-11-25 17:53:46 +0200

    some massege

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -1,10 +1,26 @@
 from wave_helper import load_wave , save_wave
+from copy import deepcopy
 import numpy as np
+import math
+
+
+def normal(wave_seq):
+    ret_seq, pair = [], []
+    for wave in wave_seq_gen(wave_seq):
+        if len(pair) == 2:
+            ret_seq.append(pair)
+            pair = []
+        wave = min(32767, wave)
+        wave = max(-32768, wave)
+        wave = int(wave)
+        pair.append(wave)
+    return ret_seq
 
 def wave_seq_gen(wave_seq):
     for leftwave, rightwave in wave_seq :
         yield leftwave
         yield rightwave
+
 
 def acceleration(wave_data):
         _ , wave_seq = wave_data
@@ -15,25 +31,59 @@ def acceleration(wave_data):
                     ret_seq.append(  pair  )
                     pair = [ ]
                 pair.append(wave)
-        return _ , ret_seq
+        return _ , normal(ret_seq)
+
 
 def slow(wave_data):
     _ , wave_seq = wave_data
     ret_seq , pair = [ ] , []
-    for index ,wave in enumerate(wave_seq_gen(wave_seq)) :
+    for index , wave in enumerate(wave_seq_gen(wave_seq)) :
         if len( pair ) == 2 :
             if index % 4 == 3 :
-                
-            ret_seq.append(  pair  )
+                ret_seq.append(  pair  )
             pair = [ ]
         pair.append(wave)
-    return _ , ret_seq
+    return _ , normal(ret_seq)
+
 
 def inversion(wave_data):
-    pass
+    _ , wave_seq = wave_data
+    wave_seq_inverted = wave_seq[::-1]
+    return _ , normal(wave_seq_inverted)
+
 
 def dimming_filter(wave_data):
-    pass
+
+
+    def calulate_average( _list, new_item  ):
+        _list.pop(0)
+        _list.append( new_item )
+        return sum(_list) / len(_list)
+
+    _ , wave_seq = wave_data
+    dimmed_wave_seq = list()
+    prevx , prevy = wave_seq[0]
+    new_seq = []
+
+    prevsx = [ wave_seq[0][i] for i in range(2)]
+    prevsx = [ sum(prevsx)/2 ] + prevsx
+    prevsy = [wave_seq[0][i] for i in range(2)]
+    prevsy = [sum(prevsy) / 2] + prevsy
+
+    for index, pair in enumerate(wave_seq[1:]):
+        dimmed_wave_seq.append([])
+        for prevs , wave in zip( [ prevsx , prevsy] , pair ):
+            dimmed_wave_seq[-1].append(
+                calulate_average(prevs , wave))
+
+    dimmed_wave_seq.append([])
+    for prevs in [ prevsx , prevsy] :
+        dimmed_wave_seq[-1].append(
+            calulate_average(prevs, sum(prevs[1:])/2))
+
+    return _, normal(dimmed_wave_seq)
+
+
 
 def stretch_volume(wave_data, factor):
     _ , wave_seq = wave_data
@@ -42,20 +92,55 @@ def stretch_volume(wave_data, factor):
         if len( pair ) == 2 :
             ret_seq.append(  pair  )
             pair = [ ]
-        wave *= factor
-        wave  = min(32767 , wave)
-        wave  = max(-32768 , wave)
-        wave  = int(wave)
         pair.append(wave)
 
-    return _ , ret_seq
+    return _ , normal(ret_seq)
 
 
 def increase_volume(wave_data):
     return stretch_volume(wave_data, 1.2)
 
+
 def decrease_volume(wave_data):
     return stretch_volume(wave_data, 1/1.2)
+
+SAMPLERATE = 2000
+MAXVOLUME  = 32767
+MINVOLUME  = -32768
+
+NOTES = {
+
+    'A' : 440,
+    'B' : 494,
+    'C' : 523,
+    'D' : 587,
+    'E' : 659,
+    'F' : 698,
+    'G' : 784,
+    'Q' : 0
+}
+
+
+def sample_rate_i(i, frequency):
+    if frequency == 0:
+        return  0
+
+    sample_per_cycle = SAMPLERATE / frequency
+    return int(  MAXVOLUME * math.sin( math.pi * 2 * i / sample_per_cycle ))
+
+
+def composite_txt_file(filename):
+    openfile = open(filename, 'r').read()
+    notes_list = list()
+    for i , note in enumerate(openfile.split()):
+        if i%2 == 0:
+            notes_list.append([])
+        notes_list[-1].append(note)
+    return notes_list
+
+
+
+
 
 if __name__ == '__main__':
     D = load_wave( "./wav Samples/batman_theme_x.wav")
@@ -63,3 +148,18 @@ if __name__ == '__main__':
     print(D[1][0])
     D = increase_volume(D)
     print(D[1][0])
+
+    D = [ 0 , [ [ i ,  i ] for i in range(10)] ]
+    a, b = dimming_filter(D)
+    print(b)
+
+
+    #save_wave(a, b, 'dimmed.wav')
+    for ( x , y) in composite_txt_file('tt1.txt'):
+        note = NOTES[x]
+        _sum = 0
+        for i in range( int(y) * 125):
+            _sum = sample_rate_i( i , note )
+            print(_sum , end= " ")
+
+        print(_sum, end=" ")
